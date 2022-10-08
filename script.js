b8fb8b60d9a5009c08ac366f09cf560299d58b6c
Amir Lamrabat <amirlamrabat1515@gmail.com>
2022-10-08 14:45:39 -0400

    Update plan for executing the game

---
+++ script.js
@@ -0,0 +1,38 @@
+// create a variable that allows user input to pick rock paper or scissors
+// create a variable that allows computer to randomly generate rock paper scissors
+// create a function declaring scenarios of what happens in losses or wint
+// put that function inside a loop to make the game play to the best of 5 series
+
+
+
+
+
+
+
+
+// functions to simulate one round of play and the winner/loser/or draw
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+// this function has to have the best of 5 series and declare a winner
+function game(playRound){
+
+}
+
+
+//these are the constants to be called inside the function that has the player and computer choice
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+
+// the user's input for the game
+function getPlayerChoice(){
+
+}
+
+// the computer's input for the game 
+function getComputerChoice(){
+
+}
+
