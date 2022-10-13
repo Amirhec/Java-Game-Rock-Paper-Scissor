8e9eefaa4df04b93529e710b1fe5ebbe8092fd99
Amir Lamrabat <amirlamrabat1515@gmail.com>
2022-10-13 14:22:11 -0400

    change algorithm and computer generation

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-// create a variable that allows user input to pick rock paper or scissors
-// create a variable that allows computer to randomly generate rock paper scissors
-// create a function declaring scenarios of what happens in losses or wint
-// put that function inside a loop to make the game play to the best of 5 series
+// had to re do my algorithm after re visiting concepts of functions and understanding parameters
+// Start the project by having a function called "getComputerChoice" randomly generating rock paper or scissor
+// make a function that takes the two parameters of the computerSelection and playerSelection and returns a string declaring the winner
+// return all values and dont console.log the scenarios in the function
+//test the player vs computer function with a constant selection of rock for the user to see if it works
+//after testing is complete allow a prompt for the user to input the data to play the game
+// use loops for the game to have a best out of 5 round
 
-
-
-
-
-
-
-
-// functions to simulate one round of play and the winner/loser/or draw
-function playRound(playerSelection, computerSelection) {
-    
-}
-
-// this function has to have the best of 5 series and declare a winner
-function game(playRound){
-
-}
-
-
-//these are the constants to be called inside the function that has the player and computer choice
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-
-// the user's input for the game
-function getPlayerChoice(){
-
-}
-
-// the computer's input for the game 
 function getComputerChoice(){
-
+    let choices = ["rock","paper","scissor"];
+    let randomChoice = choices[Math.floor(Math.random()*3)];
+    return randomChoice;
 }
 
+console.log("computer selected " + getComputerChoice());
+
+// Random computer generation works and prints to the console
